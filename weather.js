733642423f049cfa7811c9592e85e637c1f0ae61
Vlad Korobko <max.vlad.kor@gmail.com>
2022-12-24 14:12:05 +0200

    Added service, which create file for saving token, etc.

--- weather.js
+++ weather.js
@@ -1,7 +1,18 @@
 #!/usr/bin/env node
 
 import {getArgs} from './helpers/args.js';
-import {printHelp} from "./services/log.services.js";
+import {printHelp, printSuccess, printError} from "./services/log.services.js";
+import {saveKeyValue} from "./services/storage.service.js";
+
+const saveToken = async (token) => {
+    try {
+        await saveKeyValue('token', token);
+        printSuccess('Токен збережений!');
+    } catch (error) {
+        printError(error.message);
+    }
+}
+
 const initCLI = () => {
     const args = getArgs(process.argv);
 
@@ -14,7 +25,7 @@ const initCLI = () => {
     }
 
     if (args.t) {
-        // Save token
+        return saveToken(args.t)
     }
 
     //Output weather
