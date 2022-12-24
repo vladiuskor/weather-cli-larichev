8a31ea259fde46c5578c7aa9c49640186052c933
Vlad Korobko <max.vlad.kor@gmail.com>
2022-12-24 15:46:25 +0200

    Replace native https module to axios and wrote a script which makes get request to openweather and returns data

--- services/api.services.js
+++ services/api.services.js
@@ -1,5 +1,6 @@
-import https from 'https';
 import {getKeyValue, TOKEN_DICTIONARY} from "./storage.service.js";
+import axios from "axios";
+
 const getWeather = async (city) => {
     const token = await getKeyValue(TOKEN_DICTIONARY.token);
 
@@ -7,24 +8,16 @@ const getWeather = async (city) => {
         throw new Error('Не заданий ключ API, введіть його через команду -t [API_KEY]');
     }
 
-    const url = new URL('https://api.openweathermap.org/data/2.5/weather');
-
-    url.searchParams.append('q', city);
-    url.searchParams.append('appid', token);
-    url.searchParams.append('lang', 'ua');
-    url.searchParams.append('units', 'metric');
-
-    https.get(url, (response) => {
-        let res = '';
-
-        response.on('data', (chunk) => {
-            res += chunk;
-        });
-
-        response.on('end', () => {
-            console.log(res);
-        });
+    const {data} = await axios.get('https://api.openweathermap.org/data/2.5/weather', {
+        params: {
+            q: city,
+            appid: token,
+            lang: 'ua',
+            units: 'metric'
+        }
     });
+
+    return data;
 };
 
 export {
